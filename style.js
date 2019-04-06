3e1ec5083d4179c3bcd09fcf9359d5fd2b9a1147
Jesse Hitch <jessebot@linux.com>
2019-04-06 09:55:47 -0700

    I guess I need style.js now

---
+++ style.js
@@ -0,0 +1,2 @@
+// there's no non-hacky way to do this that I know of on 0 hours of sleep
+document.getElementById("jenkins-head-icon").src="https://jessebot.github.io/jenkins-dark-hell/jenkinscute.png";
