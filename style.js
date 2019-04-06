c25843f410a01e9dc0dbd00923c72cb92359c489
Jesse Hitch <jessebot@linux.com>
2019-04-06 10:02:46 -0700

    forgot stuff

--- style.js
+++ style.js
@@ -1,2 +1,6 @@
-// there's no non-hacky way to do this that I know of on 0 hours of sleep
-document.getElementById("jenkins-head-icon").src="https://jessebot.github.io/jenkins-dark-hell/jenkinscute.png";
+// ::shrug::
+$(document).ready(
+    function() {
+      document.getElementById("jenkins-head-icon").src="https://jessebot.github.io/jenkins-dark-hell/jenkinscute.png";
+    }
+);
